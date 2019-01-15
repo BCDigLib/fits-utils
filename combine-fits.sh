4ac4c9136fb812a276dda2850826832b0bbe0e62
Adam Jazairi <16103405+jazairi@users.noreply.github.com>
2019-01-15 12:09:11 -0500

    Use strings instead of external files for XML declaration and tags

--- combine-fits.sh
+++ combine-fits.sh
@@ -1,13 +1,25 @@
-workflowdocs="/Users/walkerpb/Documents/scripts/aip-mac-bash-fits-master/aip-workflowdocs/"
-fitsoutput="/Volumes/digital-library/current-projects/Anansi-Stories-d2/aips/aip-title/fits-output"
+# path to directory containing FITS files (this will also be the output dir)
+fitsdir=""
 
-echo ""
+# xml declaration to be prepended to output file
+xmldesc="<?xml version=\"1.0\" encoding=\"utf-8\"?>"
+
+# root element opening tag for output file
+rootopen="<combined-fits xmlns=\"http://hul.harvard.edu/ois/xml/ns/fits/fits_output\">"
+
+# root element closing tag for output file
+rootclose="</combined-fits>"
+
 echo "Combining FITS files..."
-echo ""
 
 # combine files into a single file and remove the XML declarations
-for file in $fitsoutput/*_fits.xml; do cat $file | egrep -v "xml version" >> $fitsoutput/body.xml; done
+for file in $fitsdir/*fits.xml; do cat $file | egrep -v "xml version" >> $fitsdir/body.xml; done
 
-# add single XML declaration and opening and closing wrapper tags
-cat $workflowdocs/open.xml $fitsoutput/body.xml $workflowdocs/close.xml > $fitsoutput/combined-fits.xml
-rm $fitsoutput/body.xml
+# add combined FITS to output file, along with single XML declaration and opening and closing wrapper tags
+echo $xmldesc >> $fitsdir/combined-fits.xml
+echo $rootopen >> $fitsdir/combined-fits.xml
+cat $fitsdir/body.xml >> $fitsdir/combined-fits.xml
+echo $rootclose >> $fitsdir/combined-fits.xml
+
+# delete working file
+rm $fitsdir/body.xml
